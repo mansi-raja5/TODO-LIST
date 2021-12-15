eb9db81ac06f7854aad8b3dd62137f71f49b0be7
Mansi Raja <mansi.raja@encora.com>
2021-12-16 00:03:19 +0530

    Setting Dynamic Inline Styles

--- src/components/GoalForm/GoalForm.js
+++ src/components/GoalForm/GoalForm.js
@@ -6,21 +6,38 @@ import "./GoalForm.css";
 const GoalForm = (props) => {
   const [enteredGoal, setEnteredGoal] = useState("");
 
+  const [isValid, setIsValid] = useState(true);
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredGoal.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredGoal);
     setEnteredGoal("");
   };
 
   const onChangeInputHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredGoal(event.target.value);
   };
 
   return (
     <form onSubmit={onSubmitHandler}>
       <div className="form-control">
-        <label>Goal</label>
-        <input type="text" value={enteredGoal} onChange={onChangeInputHandler} />
+        <label style={{ color: !isValid ? "red" : "black" }}>Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "black",
+            backgroundColor: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          value={enteredGoal}
+          onChange={onChangeInputHandler}
+        />
       </div>
       <Button buttonType="submit">Add Goal</Button>
     </form>
