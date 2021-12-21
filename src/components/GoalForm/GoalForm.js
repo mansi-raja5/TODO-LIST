2ce263079532db3528c2a9f88cd2a34c6155cde9
Mansi Raja <mansi.raja@encora.com>
2021-12-22 00:17:51 +0530

    Styled Components and Dynamic Props

--- src/components/GoalForm/GoalForm.js
+++ src/components/GoalForm/GoalForm.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Button from "../UI/Button";
-
-import "./GoalForm.css";
+import styled from "styled-components";
 
 const GoalForm = (props) => {
   const [enteredGoal, setEnteredGoal] = useState("");
@@ -25,16 +24,50 @@ const GoalForm = (props) => {
     setEnteredGoal(event.target.value);
   };
 
+  const FormControl = styled.div`
+    margin: 0.5rem 0;
+
+    & label {
+      font-weight: bold;
+      display: block;
+      margin-bottom: 0.5rem;
+    }
+
+    & input {
+      display: block;
+      width: 100%;
+      border: 1px solid #ccc;
+      font: inherit;
+      line-height: 1.5rem;
+      padding: 0 0.25rem;
+    }
+
+    & input:focus {
+      outline: none;
+      background: #fad0ec;
+      border-color: #8b005d;
+    }
+
+    &.invalid input {
+      background: rgb(230, 179, 179);
+      border: red;
+    }
+
+    &.invalid label {
+      color: red;
+    }
+  `;
+
   return (
     <form onSubmit={onSubmitHandler}>
-      <div className={`form-control ${!isValid ? ' invalid' : ''}`}>
+      <FormControl className={!isValid && " invalid"}>
         <label>Goal</label>
         <input
           type="text"
           value={enteredGoal}
           onChange={onChangeInputHandler}
         />
-      </div>
+      </FormControl>
       <Button buttonType="submit">Add Goal</Button>
     </form>
   );
